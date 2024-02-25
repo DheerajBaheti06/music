026afd81abee01b55a219ad5f2249adf0ac00cef
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-02-25 17:55:21 +0000

    navbar done

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[">
+      <h1 className="text-center text-2xl[0.96] antialiased bg-grid-white/[0.02]">Dheeraj</h1>
+    </main>
+
+  );
+}
