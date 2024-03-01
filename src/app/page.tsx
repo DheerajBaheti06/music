8d390e7f52f92606febd760664ba7ad978fc6482
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-03-01 17:11:54 +0000

    Hero Section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[">
-      <h1 className="text-center text-2xl[0.96] antialiased bg-grid-white/[0.02]">Dheeraj</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
     </main>
-
   );
 }
